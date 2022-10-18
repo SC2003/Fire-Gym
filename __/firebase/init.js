bb0850177fe515c8301a651c2f31c291d0140997
SC2003 <46285019+SC2003@users.noreply.github.com>
2022-10-18 17:11:22 +0530

    Firebase Configs

---
+++ __/firebase/init.js
@@ -0,0 +1,11 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyB3E_MCGzx5tQ8OjFjIDKMLKy_Q8LQ55GI",
+  "appId": "1:258608564694:web:383cc2dba52f72c1cb7c77",
+  "authDomain": "fire-gym.firebaseapp.com",
+  "databaseURL": "https://fire-gym-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "measurementId": "G-HCZQD8MFRF",
+  "messagingSenderId": "258608564694",
+  "projectId": "fire-gym",
+  "storageBucket": "fire-gym.appspot.com"
+});
